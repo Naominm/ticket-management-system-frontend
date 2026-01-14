0f6e325957dfe624c08185fe92fe4b6f1f0b2735
Naominm <naomimbugua349@gmail.com>
2026-01-14 16:56:46 +0300

    Fix: Remove qoutes in css variables at index ccs file

--- src/pages/authPage.tsx
+++ src/pages/authPage.tsx
@@ -1,5 +1,9 @@
 import { Box } from "@mui/material";
 
 export default function AuthPage() {
-  return <Box component="div">Welcome to the auth page</Box>;
+  return (
+    <Box component="div" sx={{ backgroundColor: "(--var--primary-color)" }}>
+      Welcome to the auth page
+    </Box>
+  );
 }
