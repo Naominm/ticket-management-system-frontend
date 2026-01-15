d11323b804d57d720b2af773a11ba1473191c9eb
Naominm <naomimbugua349@gmail.com>
2026-01-15 15:38:57 +0300

    Feat: Build the Logo component and give it an absolute positioning

--- src/pages/authPage.tsx
+++ src/pages/authPage.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import FormComponent from "../components/formComponent";
+import logo from "../assets/logoTenakata.svg";
 
 export default function AuthPage() {
   return (
@@ -7,10 +8,22 @@ export default function AuthPage() {
       component="div"
       sx={{
         backgroundColor: "var(--background-color)",
+        position: "relative",
         height: "100vh",
         gap: 5,
       }}
     >
+      <Box
+        component={"img"}
+        src={logo}
+        sx={{
+          position: "absolute",
+          top: 50,
+          left: "40%",
+          zIndex: 5174,
+          height: "15vh",
+        }}
+      ></Box>
       <FormComponent />
     </Box>
   );
