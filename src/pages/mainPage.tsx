9c142947ac976778896ea214e19753042bc99245
Naominm <naomimbugua349@gmail.com>
2026-01-27 10:46:30 +0300

    Fix: remove unwanted border red color

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -42,7 +42,6 @@ export default function CollapsibleSidebar() {
               px: 2,
               minHeight: "15vh",
               backgroundColor: "#fff",
-              border: "1px solid red",
             }}
           >
             <IconButton
