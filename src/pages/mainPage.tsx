982833d931b9309d88d223f3c167e92aecc30982
Naominm <naomimbugua349@gmail.com>
2026-01-27 09:13:41 +0300

    Style: position the cta buttons at top of the page

--- src/pages/mainPage.tsx
+++ src/pages/mainPage.tsx
@@ -1,4 +1,4 @@
-import { Box, IconButton, Typography } from "@mui/material";
+import { Box, Button, IconButton, Typography } from "@mui/material";
 import SidebarComponent from "../components/sidebarComponent";
 import SearchIcon from "@mui/icons-material/Search";
 
@@ -22,7 +22,46 @@ export default function CollapsibleSidebar() {
             </Typography>
           </IconButton>
         </Box>
-        <Box sx={{ bgcolor: "#f4f4f4", minHeight: "400px", mt: 2 }}> hello</Box>
+        <Box
+          sx={{
+            bgcolor: "#f4f4f4",
+            minHeight: "400px",
+            mt: 2,
+            display: "flex",
+            flexDirection: "column",
+            gap: 5,
+          }}
+        >
+          <Box
+            sx={{
+              mt: 5,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "300px",
+              px: 2,
+              minHeight: "15vh",
+              backgroundColor: "#fff",
+              border: "1px solid red",
+            }}
+          >
+            <IconButton
+              sx={{ fontSize: "1rem", fontFamily: "var(--primary-font)" }}
+            >
+              Tickets
+            </IconButton>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "var(--background-color)",
+                fontFamily: "Var(--primary-font)",
+                textTransform: "capitalize",
+              }}
+            >
+              Create a task
+            </Button>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
