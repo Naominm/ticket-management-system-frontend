d1d2f3c003899da4084c63b586b2c2e506feb241
Naominm <naomimbugua349@gmail.com>
2026-01-28 15:27:45 +0300

    Feat: Change the flex display of the content for better visibility

--- src/components/editStatusModal.tsx
+++ src/components/editStatusModal.tsx
@@ -31,41 +31,92 @@ export default function EditModalStatus({ open, onClose }: Props) {
         </IconButton>
       </DialogTitle>
       <DialogContent>
-        <Typography sx={{ mb: 2 }}>Choose any color and click save</Typography>
-        <Box sx={{ display: "flex", gap: 2, mb: 2 }}>
-          {[green[500], orange[500], red[500]].map((color) => (
-            <Box
-              key={color}
-              onClick={() => setSelectedColor(color)}
-              sx={{
-                width: 18,
-                height: 18,
-                borderRadius: "50%",
-                bgcolor: color,
-                cursor: "pointer",
-                outline: selectedColor === color ? "2px solid black" : "none",
-              }}
-            />
-          ))}
+        <Box sx={{ display: "flex", gap: 5 }}>
+          <Typography sx={{ mb: 2 }}>
+            Choose any color and click save
+          </Typography>
+          <Box sx={{ display: "flex", gap: 2, mb: 2 }}>
+            {[green[500], orange[500], red[500]].map((color) => (
+              <Box
+                key={color}
+                onClick={() => setSelectedColor(color)}
+                sx={{
+                  width: 18,
+                  height: 18,
+                  borderRadius: "50%",
+                  bgcolor: color,
+                  cursor: "pointer",
+                  outline: selectedColor === color ? "2px solid black" : "none",
+                }}
+              />
+            ))}
+          </Box>
         </Box>
+        <hr />
         <Box
           sx={{
             bgcolor: "#f5f5f5",
+            mt: 4,
             p: 2,
             borderRadius: 2,
             fontSize: "0.85rem",
           }}
         >
-          <Typography>🟢 Green: Task completed</Typography>
-          <Typography>🟠 Orange: Task in progress</Typography>
-          <Typography>🔴 Red: Task blocked or delayed</Typography>
+          <Typography
+            sx={{
+              fontFamily: "var(--primary-font)",
+              fontSize: "0.8rem",
+              color: "gray",
+            }}
+          >
+            🟢 Green: You can press the green button only when the task is
+            completed and resolved successfully
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "var(--primary-font)",
+              fontSize: "0.8rem",
+              color: "gray",
+            }}
+          >
+            🟠 The yellow button means that the task is still in progress. When
+            a task is created, the default status of this task is yellow
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "var(--primary-font)",
+              fontSize: "0.8rem",
+              color: "gray",
+            }}
+          >
+            🔴 If the problem is not resolved for any reason, you can press the
+            red button ,If the remaining days field is greater than the Task End
+            Date field, the status automatically changes to red
+          </Typography>
         </Box>
       </DialogContent>
       <DialogActions>
-        <Button onClick={onClose} color="inherit">
+        <Button
+          onClick={onClose}
+          color="inherit"
+          sx={{
+            bgcolor: "var(--background-color)",
+            color: "#fff",
+            fontWeight: 600,
+            fontFamily: "var(--primary-font)",
+          }}
+        >
           close
         </Button>
-        <Button variant="contained" sx={{ bgcolor: "#f99417" }}>
+        <Button
+          variant="contained"
+          sx={{
+            bgcolor: "#f99417",
+            color: "#fff",
+            fontWeight: 600,
+            fontFamily: "var(--primary-font)",
+          }}
+        >
           save
         </Button>
       </DialogActions>
