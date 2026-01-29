4f7f3c27b092b6c2352844a5ec7ad796ca202414
Naominm <naomimbugua349@gmail.com>
2026-01-29 08:26:41 +0300

    Feat: Buid the main component container

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,27 @@
 import { Box } from "@mui/material";
 import SidebarComponent from "../components/sidebarComponent";
+import SearchComponent from "../components/searchComponent";
 
 export default function DashboardPage() {
   return (
-    <Box>
+    <Box sx={{ display: "flex" }}>
       <SidebarComponent />
+      <Box sx={{ flexGrow: 1 }}>
+        <SearchComponent />
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          bgcolor: "#f4f4f4",
+          height: "auto",
+          minHeight: "600px",
+          pl: 5,
+          mt: 2,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      ></Box>
     </Box>
   );
 }
