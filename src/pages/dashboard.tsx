92a4b1038edf5f4cb6c56ead1c1739587ee0a761
Naominm <naomimbugua349@gmail.com>
2026-01-29 15:31:28 +0300

    Refactor: make the card body content a component and import it at the dashbord

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -5,14 +5,11 @@ import {
   CardContent,
   Typography,
   IconButton,
-  Button,
 } from "@mui/material";
 import SidebarComponent from "../components/sidebarComponent";
 import SearchComponent from "../components/searchComponent";
 import MapsHomeWorkIcon from "@mui/icons-material/MapsHomeWork";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import ManAvatar from "../assets/man.png";
+import CardBodyComponent from "../components/cardBodyComponent";
 export default function DashboardPage() {
   return (
     <Box sx={{ display: "flex", bgcolor: "#f4f4f4", minHeight: "100vh" }}>
@@ -71,121 +68,7 @@ export default function DashboardPage() {
                     <MapsHomeWorkIcon />
                   </IconButton>
                 </CardContent>
-                <Card>
-                  <CardContent>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        px: 0.5,
-                      }}
-                    >
-                      <Button
-                        size="small"
-                        variant="contained"
-                        sx={{
-                          fontFamily: "var(--primary-font)",
-                          bgcolor: "var(--yellow-color)",
-                          fontSize: "0.5rem",
-                        }}
-                      >
-                        in process
-                      </Button>
-                      <Typography
-                        variant="body2"
-                        sx={{
-                          fontSize: "0.6rem",
-                          fontFamily: "var(--primary-font)",
-                          color: "gray",
-                        }}
-                      >
-                        #47AD6
-                      </Typography>
-                    </Box>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        mt: 2,
-                        gap: 2,
-                      }}
-                    >
-                      <Box
-                        component={"img"}
-                        sx={{ width: "40px" }}
-                        src={ManAvatar}
-                      />
-                      <Box
-                        component={"div"}
-                        sx={{
-                          display: "flex",
-                          flexDirection: "column",
-                          gap: 0.4,
-                        }}
-                      >
-                        <Typography
-                          sx={{
-                            fontFamily: "var(--primary-font)",
-                            color: "#000",
-                            fontSize: "0.5rem",
-                            textTransform: "capitalize",
-                          }}
-                        >
-                          Ahmed Mohamoud
-                        </Typography>
-                        <Typography
-                          sx={{
-                            fontFamily: "var(--primary-font)",
-                            color: "#000",
-                            fontSize: "0.4rem",
-                            textTransform: "capitalize",
-                          }}
-                        >
-                          Invalid Status in McDonalds Company
-                        </Typography>
-                      </Box>
-                    </Box>
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "flex-end",
-                      }}
-                    >
-                      <Box sx={{ display: "flex", alignItems: "center" }}>
-                        <IconButton>
-                          <AccessTimeIcon sx={{ fontSize: "small" }} />
-                        </IconButton>
-                        <Typography
-                          variant="body2"
-                          sx={{
-                            fontFamily: "var(--primary-font)",
-                            fontSize: "0.5rem",
-                            color: "gray",
-                          }}
-                        >
-                          Feb 11.2024
-                        </Typography>
-                      </Box>
-                      <Box sx={{ display: "flex", alignItems: "center" }}>
-                        <IconButton>
-                          <CalendarTodayIcon sx={{ fontSize: "small" }} />
-                        </IconButton>
-                        <Typography
-                          variant="body2"
-                          sx={{
-                            fontFamily: "var(--primary-font)",
-                            fontSize: "0.5rem",
-                            color: "gray",
-                          }}
-                        >
-                          4.30 hrs ago
-                        </Typography>
-                      </Box>
-                    </Box>
-                  </CardContent>
-                </Card>
+                <CardBodyComponent />
               </Card>
             </Box>
             <Box
