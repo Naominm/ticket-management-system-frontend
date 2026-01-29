bdf35ff62ec9f0424129daae150ab0a42bc4e16c
Naominm <naomimbugua349@gmail.com>
2026-01-29 10:46:39 +0300

    Feat: Structure all the components at the dashboard main section

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,29 +1,76 @@
-import { Box } from "@mui/material";
+import { Box, Paper } from "@mui/material";
 import SidebarComponent from "../components/sidebarComponent";
 import SearchComponent from "../components/searchComponent";
 
 export default function DashboardPage() {
   return (
-    <Box>
+    <Box sx={{ bgcolor: "#f4f4f4", pb: 5 }}>
       <SidebarComponent />
-      <Box sx={{ display:"flex",flexDirection:"column", ml:20}}>
+      <Box sx={{ display: "flex", flexDirection: "column", ml: 20 }}>
         <SearchComponent />
-      
-      <Box
-        sx={{
-          bgcolor: "#f4f4f4",
-          width:"100%",
-          height: "auto",
-          minHeight: "600px",
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-    <Box sx={{display:"flex", width:"100%"}}>
-        <Box sx={{display:"flex", flexDirection:"column", gap:2, width:"25%" ,height:"auto"}}>the past tickets will show here</Box>
-    <Box sx={{display:"flex", width:"75%", heigth:"auto"}}></Box>
-    </Box>    
-      </Box>
+
+        <Box
+          sx={{
+            bgcolor: "#f4f4f4",
+            width: "100%",
+            height: "auto",
+            minHeight: "600px",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <Box sx={{ display: "flex", width: "100%", mt: 4, gap: 2, px: 2 }}>
+            <Box
+              component={Paper}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+                width: "35%",
+                height: "auto",
+                minHeight: "100vh",
+              }}
+            >
+              Last Tickets
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                width: "100%",
+                gap: 2,
+              }}
+            >
+              <Box sx={{ display: "flex", gap: 2, height: "50vh" }}>
+                <Box
+                  component={Paper}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 2,
+                    width: "50%",
+                  }}
+                >
+                  Most Active employee
+                </Box>
+                <Box
+                  component={Paper}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: 2,
+                    width: "50%",
+                  }}
+                >
+                  Most Active client
+                </Box>
+              </Box>
+              <Box component={Paper} sx={{ width: "100%", height: "50vh" }}>
+                component 2
+              </Box>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
