98989df278ac7a6f46889753540e8107e72f33b2
Naominm <naomimbugua349@gmail.com>
2026-02-01 06:17:42 +0300

    Feat: import the search and the sidebar component at settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,4 +1,24 @@
 import { Box } from "@mui/material";
+import SidebarComponent from "../components/sidebarComponent";
+import SearchComponent from "../components/searchComponent";
 export default function SettingsPage() {
-  return <Box> this is the settings page</Box>;
+  return (
+    <Box sx={{ display: "flex", bgcolor: "#f4f4f4", minHeight: "100vh" }}>
+      <SidebarComponent />
+
+      <Box component="main" sx={{ flexGrow: 1, pb: 5 }}>
+        <SearchComponent />
+
+        <Box
+          sx={{
+            bgcolor: "#f4f4f4",
+            width: "100%",
+            height: "auto",
+            minHeight: "200vh",
+            display: "flex",
+          }}
+        ></Box>
+      </Box>
+    </Box>
+  );
 }
