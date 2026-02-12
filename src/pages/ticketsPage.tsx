eb1b0924ce986569cda9dec17a75620dc9d8be77
Naominm <naomimbugua349@gmail.com>
2026-02-12 16:03:49 +0300

    Fix: Use the correct name to avoid a naming mismatch

--- src/pages/ticketsPage.tsx
+++ src/pages/ticketsPage.tsx
@@ -122,7 +122,7 @@ export default function TicketPage() {
                     {tickets.map((ticket: any) => (
                       <TableRow key={ticket.id}>
                         <TableCell>{ticket.id}</TableCell>
-                        <TableCell>{ticket.user?.name || "N/A"}</TableCell>
+                        <TableCell>{ticket.user?.lastName || "N/A"}</TableCell>
                         <TableCell>
                           {new Date(ticket.createdAt).toLocaleDateString()}
                         </TableCell>
